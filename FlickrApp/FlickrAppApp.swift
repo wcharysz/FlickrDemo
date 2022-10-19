8e7abe9eda47099bda1eb240e347467316fffe64
Wojciech Charysz <w.charysz@appdev.de>
2022-10-19 10:50:54 +0200

    Initial Commit

---
+++ FlickrApp/FlickrAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlickrAppApp.swift
+//  FlickrApp
+//
+//  Created by Wojciech Charysz on 19.10.22.
+//
+
+import SwiftUI
+
+@main
+struct FlickrAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
